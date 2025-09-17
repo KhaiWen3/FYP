533610c7b84aba09c7cf5e4d068b40611d5710cf
KhaiWen3 <kwen0310098@gmail.com>
2025-09-17 18:52:05 +0800

    update helper

---
+++ PatientAppointmentSchedulingSystem/PatientAppointmentSchedulingSystem/Helpers/FirebaseStorageHelper.cs
@@ -0,0 +1,46 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+
+namespace PatientAppointmentSchedulingSystem.Helpers
+{
+    public class FirebaseStorageHelper
+    {
+        private readonly StorageClient _storageClient; 
+        private readonly string _bucketName = "fyp1-uwp.appspot.com";
+
+        public FirebaseStorageHelper()
+        {
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.FromFile("wwwroot/firebase-adminsdk.json")
+                });
+            }
+            _storageClient = StorageClient.Create(GoogleCredential.FromFile("wwwroot/firebase-adminsdk.json"));
+        }
+
+        //Upload image & return its public url
+        public async Task<string> UploadImageAsync(Stream fileStream, string fileName, string folderName, string contentType="image/jpeg")
+        {
+            //upload into "DoctorImage" folder
+            var objectName = $"{folderName}/{fileName}";
+            await _storageClient.UploadObjectAsync(_bucketName, objectName, contentType, fileStream);
+            return GetImageUrl (fileName, folderName); 
+        }
+
+        //Delete image by filename
+        public async Task DeleteImageAsync(string fileName, string folderName)
+        {
+            var objectName = $"{folderName}/{fileName}";
+            await _storageClient.DeleteObjectAsync(_bucketName, objectName);
+        }
+
+        //generate the public download url
+        public string GetImageUrl(string fileName, string folderName) 
+        { 
+            return $"https://firebasestorage.googleapis.com/v0/b/{_bucketName}/o/{Uri.EscapeDataString(folderName + "/" + fileName)}?alt=media";
+        }
+    }
+}
---
+++ PatientAppointmentSchedulingSystem/PatientAppointmentSchedulingSystem/Helpers/JitsiHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace PatientAppointmentSchedulingSystem.Helpers
+{
+    public class JitsiHelper
+    {
+        public static string GenerateMeetingLink(string topic)
+        {
+            string roomName = $"{topic}-{Guid.NewGuid()}";
+            return $"https://meet.jit.si/{roomName}";
+        }
+    }
+}
